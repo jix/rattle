1856b54f100c57f9da0d07aa9bc18caaf501c0d0
Jannis Harder <jix@jixco.de>
2016-07-14 16:49:52 +0200

    Tests for signal access checks

---
+++ tests/test_signal_access.py
@@ -0,0 +1,84 @@
+from pytest import raises
+from rattle.signal import *
+from rattle.module import *
+from rattle.type import *
+from rattle.error import *
+
+
+def test_wire_invalid_read():
+    class Mod1(Module):
+        def construct(self):
+            self.wire = Wire(Bool)
+
+    class Mod2(Module):
+        def construct(self):
+            self.mod1 = Mod1()
+            self.wire = Wire(Bool)
+            with raises(InvalidSignalRead):
+                self.wire[:] = self.mod1.wire
+    Mod2()
+
+
+def test_wire_invalid_assignment():
+    class Mod1(Module):
+        def construct(self):
+            self.wire = Wire(Bool)
+
+    class Mod2(Module):
+        def construct(self):
+            self.mod1 = Mod1()
+            self.wire = Wire(Bool)
+            with raises(InvalidSignalAssignment):
+                self.mod1.wire[:] = self.wire
+    Mod2()
+
+
+def test_input_invalid_read():
+    class Mod1(Module):
+        def construct(self):
+            self.input = Input(Bool)
+
+    class Mod2(Module):
+        def construct(self):
+            self.mod1 = Mod1()
+            self.wire = Wire(Bool)
+            with raises(InvalidSignalRead):
+                self.wire[:] = self.mod1.input
+    Mod2()
+
+
+def test_input_invalid_assignment():
+    class Mod(Module):
+        def construct(self):
+            self.input = Input(Bool)
+            with raises(InvalidSignalAssignment):
+                self.input[:] = self.input
+    Mod()
+
+
+def test_output_invalid_read():
+    class Mod(Module):
+        def construct(self):
+            self.output = Output(Bool)
+            with raises(InvalidSignalRead):
+                self.output[:] = self.output
+    Mod()
+
+
+def test_output_invalid_assignment():
+    class Mod1(Module):
+        def construct(self):
+            self.output = Output(Bool)
+
+    class Mod2(Module):
+        def construct(self):
+            self.mod1 = Mod1()
+            self.wire = Wire(Bool)
+            with raises(InvalidSignalAssignment):
+                self.mod1.output[:] = self.wire
+    Mod2()
+
+
+def test_constant_invalid_assignment(module):
+    with raises(InvalidSignalAssignment):
+        Bool[True][:] = Bool[False]
