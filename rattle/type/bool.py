54766d4d42a3ad7bf4f911286feac963a4a6377d
Jannis Harder <jix@jixco.de>
2016-07-16 12:42:28 +0200

    Binary ops for Bool signals

--- rattle/type/bool.py
+++ rattle/type/bool.py
@@ -25,6 +25,36 @@ class BoolMixin(SignalMixin):
         self._access_read()
         return Value(Bool, expr.Not(self))
 
+    def _binary_bitop(self, other, op, const_op):
+        try:
+            other = self.signal_type.convert(other, implicit=True)
+        except ConversionNotImplemented:
+            return NotImplemented
+        self._access_read()
+        other._access_read()
+        if isinstance(self, Const) and isinstance(other, Const):
+            return Bool[const_op(self.value, other.value)]
+        else:
+            return Value(Bool, op(self, other))
+
+    def __and__(self, other):
+        return self._binary_bitop(other, expr.And, lambda a, b: a & b)
+
+    def __rand__(self, other):
+        return self.__and__(other)
+
+    def __or__(self, other):
+        return self._binary_bitop(other, expr.Or, lambda a, b: a | b)
+
+    def __ror__(self, other):
+        return self.__or__(other)
+
+    def __xor__(self, other):
+        return self._binary_bitop(other, expr.Xor, lambda a, b: a ^ b)
+
+    def __rxor__(self, other):
+        return self.__xor__(other)
+
 BoolType.signal_mixin = BoolMixin
 
 
---
+++ tests/type/test_bool_basic_ops.py
@@ -0,0 +1,36 @@
+import pytest
+from rattle.module import *
+from rattle.signal import *
+from rattle.type import *
+
+
+@pytest.mark.parametrize("value", [True, False])
+def test_const_negate(value):
+    assert (~Bool[value]).value == (not value)
+
+
+@pytest.mark.parametrize("a", [True, False])
+@pytest.mark.parametrize("b", [True, False])
+def test_const_basic_binops(a, b):
+    assert (Bool[a] & Bool[b]).value == a & b
+    assert (Bool[a] ^ Bool[b]).value == a ^ b
+    assert (Bool[a] | Bool[b]).value == a | b
+
+
+# TODO Add simulation tests when ready
+
+def test_construct_negate(module):
+    module.input = Input(Bool)
+    module.output = Output(Bool)
+    module.output[:] = ~module.input
+
+
+def test_construct_binops(module):
+    module.a = Input(Bool)
+    module.b = Input(Bool)
+    module.out_and = Output(Bool)
+    module.out_and[:] = module.a & module.b
+    module.out_or = Output(Bool)
+    module.out_or[:] = module.a | module.b
+    module.out_xor = Output(Bool)
+    module.out_xor[:] = module.a ^ module.b
