2b133e59928451a767f4c3bcd063b0ab0a84c768
Jannis Harder <jix@jixco.de>
2016-07-27 18:02:41 +0200

    Make the Clock signal type a Bundle
    Clock is now a subtype of Bundle.  It contains a Bool signal clk and optionally
    a reset signal (by default), and a clk_en signal (if gated).  It also stores a
    flag whether a reset should be performed using initial statements (for
    simulation and FPGAs).

--- rattle/type/clock.py
+++ rattle/type/clock.py
@@ -1,14 +1,50 @@
 from .type import *
+from .bool import Bool
+from .bundle import *
 
 
-# TODO Different clock types for sync/async and with or without initial
-class ClockType(SignalType):
-    def __repr__(self):
-        return "Clock"
+class Clock(Bundle):
+    def __init__(self, has_reset='sync', is_gated=False, initial_reset=True):
+        is_gated = bool(is_gated)
+        initial_reset = bool(initial_reset)
+        if has_reset != 'async':
+            has_reset = 'sync' if bool(has_reset) else False
+
+        self._has_reset = has_reset
+        self._is_gated = is_gated
+        self._initial_reset = initial_reset
+
+        layout = {'clk': Bool}
+        if has_reset:
+            layout['reset'] = Bool
+        if is_gated:
+            layout['clk_en'] = Bool
+
+        super().__init__(**layout)
+
+    @property
+    def has_reset(self):
+        return self._has_reset
+
+    @property
+    def is_gated(self):
+        return self._is_gated
+
+    @property
+    def initial_reset(self):
+        return self._initial_reset
 
     @property
     def _signature_tuple(self):
-        return (type(self),)
+        return (
+            type(self), self.has_reset, self.is_gated, self.initial_reset)
 
-Clock = ClockType()
-ClockType.__new__ = lambda cls: Clock
+    def __repr__(self):
+        parts = []
+        if self.has_reset != 'sync':
+            parts.append('has_reset=%r' % self.has_reset)
+        if self.is_gated:
+            parts.append('is_gated=%r' % self.is_gated)
+        if not self.initial_reset:
+            parts.append('initial_reset=%r' % self.initial_reset)
+        return 'Clock(%s)' % ', '.join(parts)
