24b7cdd7eab1815326d6d373e1be7bfae5ec9ba1
Jannis Harder <jix@jixco.de>
2017-03-24 15:49:48 +0100

    Port signal type for IO with handshaking

---
+++ rattle/std/port/port.py
@@ -0,0 +1,35 @@
+from rattle.prelude import *
+from rattle.type.bundle import BundleSignal
+
+
+class Port(Bundle):
+    def __init__(self, payload_type):
+        super().__init__(payload=payload_type, ready=Flip(Bool), valid=Bool)
+        self.__payload_type = payload_type
+
+    def __repr__(self):
+        return "Port(%r)" % self.__payload_type
+
+    @property
+    def payload_type(self):
+        return self.__payload_type
+
+    @property
+    def _signal_class(self):
+        return PortSignal
+
+
+class PortSignal(BundleSignal):
+    def _getitem_raw(self, key):
+        try:
+            return super()._getitem_raw(key)
+        except KeyError:
+            try:
+                return self.payload._getitem_raw(key)
+            except KeyError:
+                pass
+            raise
+
+    @property
+    def active(self):
+        return self.valid & self.ready
